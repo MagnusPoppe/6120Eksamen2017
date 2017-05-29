2b2c91201dced00fcb11bfdeb08384c1f10c077d
MagnusPoppe <Magnus.Poppe.Wang@gmail.com>
2017-05-29 10:19:39 +0200

    initial commit

---
+++ app/src/main/java/no/byteme/magnuspoppe/eksamen/ActivityMain.java
@@ -0,0 +1,15 @@
+package no.byteme.magnuspoppe.eksamen;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityMain extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/no/byteme/magnuspoppe/eksamen/FragmentMap.java
@@ -0,0 +1,33 @@
+package no.byteme.magnuspoppe.eksamen;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Dette fragmentet inneholder kartet som vises på
+ * forsiden av appen.
+ */
+public class FragmentMap extends Fragment
+{
+    public FragmentMap()
+    {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+}
